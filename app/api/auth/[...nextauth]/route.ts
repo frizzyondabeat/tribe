7a145689a0530abecaf07ee95aa5c049fbbdf871
Efeoghene Omonigho <omonigho.efeoghene@outlook.com>
2023-10-18 19:54:58 +0100

    Fixed commit issues

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -73,7 +73,7 @@ const authOptions: NextAuthOptions = {
     }
 }
 
-export const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 
 export {handler as GET, handler as POST}
 
