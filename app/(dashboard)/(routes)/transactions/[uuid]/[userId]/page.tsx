af8c8c87f92e532f5723fdcfff7a9cd8a6dcd89d
JayrushBoss <jayrushboss@gmail.com>
2023-10-20 03:13:50 -0700

    transaction details update

--- app/(dashboard)/(routes)/transactions/[uuid]/[userId]/page.tsx
+++ app/(dashboard)/(routes)/transactions/[uuid]/[userId]/page.tsx
@@ -47,103 +47,103 @@ const TransactionDetailsPage = ({params}: { params: { uuid: string, userId: stri
                 <Card className="col-span-full">
                     <CardHeader>
                         <CardTitle>
-                            Transactions Details
+                            Transaction Detail
                         </CardTitle>
                     </CardHeader>
                     <CardContent className="grid gap-4 grid-cols-2">
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Transaction ID</h1>
-                            <p className="w-full">{transaction && transaction.uuid}</p>
+                            <h1 className="w-full font-bold text-sm">Transaction ID</h1>
+                            <p className="w-full text-sm">{transaction && transaction.uuid}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Transaction Status</h1>
-                            <p className="w-full">{transaction && transaction.transactionStatus}</p>
+                            <h1 className="w-full font-bold text-sm">Transaction Status</h1>
+                            <p className="w-full text-sm">{transaction && transaction.transactionStatus}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Transaction Reference</h1>
-                            <p className="w-full">{transaction && transaction.transactionReference}</p>
+                            <h1 className="w-full font-bold text-sm">Transaction Reference</h1>
+                            <p className="w-full text-sm">{transaction && transaction.transactionReference}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Source Account</h1>
-                            <p className="w-full">{transaction && transaction.sourceAccount}</p>
+                            <h1 className="w-full font-bold text-sm">Source Account</h1>
+                            <p className="w-full text-sm">{transaction && transaction.sourceAccount}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Source Status</h1>
-                            <p className="w-full">{transaction && transaction.sourceStatus}</p>
+                            <h1 className="w-full font-bold text-sm">Source Status</h1>
+                            <p className="w-full text-sm">{transaction && transaction.sourceStatus}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Source Processor</h1>
-                            <p className="w-full">{transaction && transaction.sourceProcessor}</p>
+                            <h1 className="w-full font-bold text-sm">Source Processor</h1>
+                            <p className="w-full text-sm">{transaction && transaction.sourceProcessor}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Source Currency</h1>
-                            <p className="w-full">{transaction && transaction.sourceCurrency}</p>
+                            <h1 className="w-full font-bold text-sm">Source Currency</h1>
+                            <p className="w-full text-sm">{transaction && transaction.sourceCurrency}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Destination Account</h1>
-                            <p className="w-full">{transaction && transaction.destinationAccount}</p>
+                            <h1 className="w-full font-bold text-sm">Destination Account</h1>
+                            <p className="w-full text-sm">{transaction && transaction.destinationAccount}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Destination Status</h1>
-                            <p className="w-full">{transaction && transaction.destinationStatus}</p>
+                            <h1 className="w-full font-bold text-sm">Destination Status</h1>
+                            <p className="w-full text-sm">{transaction && transaction.destinationStatus}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Destination Processor</h1>
-                            <p className="w-full">{transaction && transaction.destinationProcessor}</p>
+                            <h1 className="w-full font-bold text-sm">Destination Processor</h1>
+                            <p className="w-full text-sm">{transaction && transaction.destinationProcessor}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Destination Currency</h1>
-                            <p className="w-full">{transaction && transaction.destinationCurrency}</p>
+                            <h1 className="w-full font-bold text-sm">Destination Currency</h1>
+                            <p className="w-full text-sm">{transaction && transaction.destinationCurrency}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Recipient Name</h1>
-                            <p className="w-full">{transaction && transaction.recipientName}</p>
+                            <h1 className="w-full font-bold text-sm">Recipient Name</h1>
+                            <p className="w-full text-sm">{transaction && transaction.recipientName}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Payment Type</h1>
-                            <p className="w-full">{transaction && transaction.paymentType}</p>
+                            <h1 className="w-full font-bold text-sm">Payment Type</h1>
+                            <p className="w-full text-sm">{transaction && transaction.paymentType}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Amount</h1>
-                            <p className="w-full">{transaction && transaction.amount}</p>
+                            <h1 className="w-full font-bold text-sm">Amount</h1>
+                            <p className="w-full text-sm">{transaction && transaction.amount}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">User ID</h1>
-                            <p className="w-full">{transaction && transaction.userId}</p>
+                            <h1 className="w-full font-bold text-sm">User ID</h1>
+                            <p className="w-full text-sm">{transaction && transaction.userId}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Institution ID</h1>
-                            <p className="w-full">{transaction && transaction.institutionId}</p>
+                            <h1 className="w-full font-bold text-sm">Institution ID</h1>
+                            <p className="w-full text-sm">{transaction && transaction.institutionId}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Activity Status</h1>
-                            <p className="w-full">{transaction && transaction.activityStatus}</p>
+                            <h1 className="w-full font-bold text-sm">Activity Status</h1>
+                            <p className="w-full text-sm">{transaction && transaction.activityStatus}</p>
                         </div>
 
                         <div className="flex items-center flex-col gap-1">
-                            <h1 className="w-full font-semibold">Narration</h1>
-                            <p className="w-full">{transaction && transaction.narration}</p>
+                            <h1 className="w-full font-bold text-sm">Narration</h1>
+                            <p className="w-full text-sm">{transaction && transaction.narration}</p>
                         </div>
 
                         <div className="flex items-center flex-col">
-                            <h1 className="w-full font-semibold">Date Created</h1>
-                            <p className="w-full">{transaction && new Date(transaction?.dateCreated).toLocaleDateString("en-US", {
+                            <h1 className="w-full font-bold text-sm">Date Created</h1>
+                            <p className="w-full text-sm">{transaction && new Date(transaction?.dateCreated).toLocaleDateString("en-US", {
                                 weekday: "long",
                                 year: "numeric",
                                 month: "long",
