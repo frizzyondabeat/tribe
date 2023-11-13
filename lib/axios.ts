faf84229a6dc001dd7f06e3fd5765e5e863168d7
Efeoghene Omonigho <omonigho.efeoghene@outlook.com>
2023-11-13 18:46:37 +0100

    BaseURL change

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://2ae3-2001-569-52c4-d700-d5fa-ec78-7080-d0ea.ngrok-free.app";
+const BASE_URL = "https://448d-2001-569-52c4-d700-7956-3158-1ffe-4528.ngrok-free.app";
 
 export default axios.create({
     baseURL: BASE_URL,
